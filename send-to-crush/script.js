3282a2433b1e5c9c8b88ef218ad5ed8a9a7abb55
theksheedz <48700059+theksheedz@users.noreply.github.com>
2024-11-22 12:24:34 -0500

    Update script.js

--- send-to-crush/script.js
+++ send-to-crush/script.js
@@ -6,7 +6,7 @@ const gif = document.querySelector(".gif");
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "I love you tooosssss <3";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3J5bmUybzZ6d3B5czJhaHJpc2Z2M3hybHh2bG16MHJod3pja2Z1NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ha6vsupXjJVMZ4WJsT/giphy.gif";
+  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWplaGpmbTgxZWliMmNob2Ywc24wMXQ4czVlcTAwOGcyNGdvdXJzeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l2Jhok92mZ2PZHjDG/giphy.gif";
 });
 
 // Make the No button move randomly on hover
